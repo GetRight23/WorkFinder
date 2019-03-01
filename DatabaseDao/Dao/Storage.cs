155509afa4018a625b6821c644075846f8b4f0ef
omazalov <ivasilich23@gmail.com>
2019-03-01 23:22:46 +0200

    added storage class

---
+++ DatabaseDao/Dao/Storage.cs
@@ -0,0 +1,47 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseDao
+{
+    public class Storage
+    {
+        private static ApplicationContext ctx = new ApplicationContext();
+        private static Storage instance = null;
+
+        private Storage()
+        {
+            AddressDao = new DatabaseDao<Address>(ctx, ctx.Address);
+            CityDao = new DatabaseDao<City>(ctx, ctx.City);
+            CityDistrictsDao = new DatabaseDao<CityDistricts>(ctx, ctx.CityDistricts);
+            FeedbackDao = new DatabaseDao<Feedback>(ctx, ctx.Feedback);
+            OrderListDao = new DatabaseDao<Orderslist>(ctx, ctx.Orderslist);
+            OrderTableDao = new DatabaseDao<OrderTable>(ctx, ctx.OrderTable);
+            ProfCategoryDao = new DatabaseDao<ProfCategory>(ctx, ctx.ProfCategory);
+            ProfesionDao = new DatabaseDao<Profession>(ctx, ctx.Profession);
+            ServiceDao = new DatabaseDao<Service>(ctx, ctx.Service);
+            WorkerDao = new DatabaseDao<Worker>(ctx, ctx.Worker);
+            OrderToServiceDao = new OrderToServiceDao(ctx, ctx.OrderToService);
+        }
+
+        public static Storage getInstance()
+        {
+            if (instance == null)
+                instance = new Storage();
+            return instance;
+        }
+
+        public DatabaseDao<Address> AddressDao { get; private set; }
+        public DatabaseDao<City> CityDao { get; private set; }
+        public DatabaseDao<CityDistricts> CityDistrictsDao { get; private set; }
+        public DatabaseDao<Feedback> FeedbackDao { get; private set; }
+        public DatabaseDao<Orderslist> OrderListDao { get; private set; }
+        public DatabaseDao<OrderTable> OrderTableDao { get; private set; }
+        public DatabaseDao<ProfCategory> ProfCategoryDao { get; private set; }
+        public DatabaseDao<Profession> ProfesionDao { get; private set; }
+        public DatabaseDao<Service> ServiceDao { get; private set; }
+        public DatabaseDao<Worker> WorkerDao { get; private set; }
+        public OrderToServiceDao OrderToServiceDao { get; set; }
+    }
+}
--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -13,23 +13,16 @@ namespace TestApplication
 		static void Main(string[] args)
 		{
 			Console.OutputEncoding = Encoding.UTF8;
-			
-			ApplicationContext ctx = new ApplicationContext();
+            Storage storage = Storage.getInstance();
 
-			OrderToServiceDao orderToServiceDao = new OrderToServiceDao(ctx, ctx.OrderToService);
-			DatabaseDao<Address> addressDao = new DatabaseDao<Address>(ctx, ctx.Address);
-			DatabaseDao<City> cityDao = new DatabaseDao<City>(ctx, ctx.City);
-			DatabaseDao<CityDistricts> cityDistrictsDao = new DatabaseDao<CityDistricts>(ctx, ctx.CityDistricts);
-			DatabaseDao<Feedback> feedbackDao = new DatabaseDao<Feedback>(ctx, ctx.Feedback);
-			DatabaseDao<Orderslist> orderListDao = new DatabaseDao<Orderslist>(ctx, ctx.Orderslist);
-			DatabaseDao<OrderTable> orderTableDao = new DatabaseDao<OrderTable>(ctx, ctx.OrderTable);
-			DatabaseDao<ProfCategory> profCategoryDao = new DatabaseDao<ProfCategory>(ctx, ctx.ProfCategory);
-			DatabaseDao<Profession> profesionDao = new DatabaseDao<Profession>(ctx, ctx.Profession);
-			DatabaseDao<Service> serviceDao = new DatabaseDao<Service>(ctx, ctx.Service);
-			DatabaseDao<Worker> workerDao = new DatabaseDao<Worker>(ctx, ctx.Worker);
-			Console.WriteLine("Done");
-			Console.ReadKey();
-			ctx.Database.CloseConnection();
-		}
+            var temp = storage.CityDao.selectEntities();
+            foreach (var item in temp)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine("Done");
+			Console.ReadKey();           
+        }
 	}
 }
