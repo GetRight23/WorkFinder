e01d893fe76bef8ed404a4a4654ab2702afbe701
omazalov <ivasilich23@gmail.com>
2019-03-02 00:08:18 +0200

    json converter added

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using DatabaseDao;
 using System.Collections.Generic;
+using JSONConvertor;
 
 namespace TestApplication
 {
@@ -15,11 +16,14 @@ namespace TestApplication
 			Console.OutputEncoding = Encoding.UTF8;
             Storage storage = Storage.getInstance();
 
+
             var temp = storage.CityDao.selectEntities();
+            JsonConvertor jsonConvertor = new JsonConvertor();
             foreach (var item in temp)
             {
-                Console.WriteLine(item.Name);
+                Console.WriteLine(jsonConvertor.toJson(item).ToString());
             }
+
 
             Console.WriteLine("Done");
 			Console.ReadKey();           
