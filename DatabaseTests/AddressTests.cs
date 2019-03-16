09369f2ba0bc9ee2ecad8fba0c8c468d509f7ea3
kovalainen <bogdan931013@gmail.com>
2019-03-16 20:36:09 +0200

    added worker tests

---
+++ DatabaseTests/AddressTests.cs
@@ -0,0 +1,98 @@
+﻿using NUnit.Framework;
+using Models;
+using DatabaseDao;
+using DatabaseConfiguration;
+using System.Data.Common;
+using System.Collections.Generic;
+
+namespace DatabaseTests
+{
+	class AddressTests
+	{
+		private static Storage Storage { get; set; }
+		private static DbConnection Connection { get; set; }
+		private static DatabaseDao<City> CityDao { get; set; }
+		private static DatabaseDao<CityDistricts> CityDistrictsDao { get; set; }
+		private static DatabaseDao<Address> AddressDao { get; set; }
+		private static City city = null;
+		private static CityDistricts district = null;
+		private static Address address = null;
+
+		[OneTimeSetUp]
+		public void Setup()
+		{
+			Storage = new PostgreStorage(Configuration.TestConnection);
+
+			Assert.IsNotNull(Storage.Connection);
+			Assert.IsNotNull(Storage.Database);
+			Assert.IsNotNull(Storage.CityDao);
+			Assert.IsNotNull(Storage.CityDistrictsDao);
+			Assert.IsNotNull(Storage);
+
+			Connection = Storage.Connection;
+			CityDao = Storage.CityDao;
+			CityDistrictsDao = Storage.CityDistrictsDao;
+			AddressDao = Storage.AddressDao;
+
+			Connection.Open();
+			Storage.createCityTable();
+			Storage.createCityDistrictsTable();
+			Storage.createAddressTable();
+		}
+
+		[OneTimeTearDown]
+		public void CleanUp()
+		{
+			var command = Connection.CreateCommand();
+			command.CommandText =	"drop table if exists address;" +
+									"drop table if exists city_districts;" +
+									"drop table if exists city";
+			command.ExecuteNonQuery();
+			Connection.Close();
+		}
+
+		[Test, Order(1)]
+		public void insertTest()
+		{
+			city = new City() { Name = "Kyiv" };
+			int cityId = CityDao.insertEntity(city);
+
+			district = new CityDistricts() { IdCity = cityId, Name = "South" };
+			int districtId = CityDistrictsDao.insertEntity(district);
+
+			address = new Address() { IdCity = cityId, IdCityDistrict = districtId, ApptNum = "5", StreetName = "Lenina" };
+			int addressId = AddressDao.insertEntity(address);
+
+			Assert.IsTrue(addressId != 0);
+		}
+
+		[Test, Order(2)]
+		public void selectTest()
+		{
+			List<Address> addresses = AddressDao.selectEntities();
+			Assert.IsTrue(addresses.Count == 1);
+		}
+
+		[Test, Order(3)]
+		public void updateTest()
+		{
+			Assert.IsNotNull(address);
+			address.StreetName = "Artema";
+
+			bool result = AddressDao.updateEntity(address);
+
+			Assert.IsTrue(result);
+			Assert.IsTrue(AddressDao.selectEntityById(address.Id).StreetName == "Artema");
+		}
+
+		[Test, Order(4)]
+		public void deleteTest()
+		{
+			Assert.IsNotNull(address);
+			bool result = AddressDao.deleteEntityById(address.Id);
+
+			Assert.IsTrue(result);
+			Assert.IsTrue(AddressDao.selectEntities().Count == 0);
+		}
+	}
+}
---
+++ DatabaseTests/WorkerTests.cs
@@ -0,0 +1,133 @@
+﻿using NUnit.Framework;
+using Models;
+using DatabaseDao;
+using DatabaseConfiguration;
+using System.Data.Common;
+using System.Collections.Generic;
+
+namespace DatabaseTests
+{
+	class WorkerTests
+	{
+		private static Storage Storage { get; set; }
+		private static DbConnection Connection { get; set; }
+		private static DatabaseDao<City> CityDao { get; set; }
+		private static DatabaseDao<CityDistricts> CityDistrictsDao { get; set; }
+		private static DatabaseDao<Address> AddressDao { get; set; }
+		private static DatabaseDao<Worker> WorkerDao { get; set; }
+		private static DatabaseDao<User> UserDao { get; set; }
+		private static City city = null;
+		private static Worker worker = null;
+		private static CityDistricts district = null;
+		private static Address address = null;
+		private static User user = null;
+
+		[OneTimeSetUp]
+		public void Setup()
+		{
+			Storage = new PostgreStorage(Configuration.TestConnection);
+
+			Assert.IsNotNull(Storage.Connection);
+			Assert.IsNotNull(Storage.Database);
+			Assert.IsNotNull(Storage.CityDao);
+			Assert.IsNotNull(Storage.CityDistrictsDao);
+			Assert.IsNotNull(Storage);
+
+			Connection = Storage.Connection;
+			CityDao = Storage.CityDao;
+			CityDistrictsDao = Storage.CityDistrictsDao;
+			AddressDao = Storage.AddressDao;
+			WorkerDao = Storage.WorkerDao;
+			UserDao = Storage.UserDao;
+
+			Connection.Open();
+			Storage.createCityTable();
+			Storage.createCityDistrictsTable();
+			Storage.createAddressTable();
+			Storage.createUserTable();
+			Storage.createWorkerTable();
+		}
+
+		[OneTimeTearDown]
+		public void CleanUp()
+		{
+			var command = Connection.CreateCommand();
+			command.CommandText =	"drop table if exists worker;" +
+									"drop table if exists address;" +
+									"drop table if exists city_districts;" +
+									"drop table if exists city;" +
+									"drop table if exists user_table";
+			command.ExecuteNonQuery();
+			Connection.Close();
+		}
+
+		[Test, Order(1)]
+		public void insertTest()
+		{
+			city = new City() { Name = "Kyiv" };
+			int cityId = CityDao.insertEntity(city);
+
+			district = new CityDistricts() { IdCity = cityId, Name = "South" };
+			int districtId = CityDistrictsDao.insertEntity(district);
+
+			address = new Address()
+			{
+				IdCity = cityId,
+				IdCityDistrict = districtId,
+				ApptNum = "5",
+				StreetName = "Lenina"
+			};
+
+			int addressId = AddressDao.insertEntity(address);
+
+			user = new User()
+			{
+				Login = "yakrut",
+				Password = "124124124",
+			};
+			int userId = UserDao.insertEntity(user);
+
+			worker = new Worker()
+			{
+				IdAddress = addressId,
+				Info = "noname",
+				Name = "Ivan",
+				LastName = "Ivanov",
+				PhoneNumber = "1241242",
+				IdUser = userId
+			};
+			int workerId = WorkerDao.insertEntity(worker);
+
+			Assert.IsTrue(workerId != 0);
+		}
+
+		[Test, Order(2)]
+		public void selectTest()
+		{
+			List<Worker> workers = WorkerDao.selectEntities();
+			Assert.IsTrue(workers.Count == 1);
+		}
+
+		[Test, Order(3)]
+		public void updateTest()
+		{
+			Assert.IsNotNull(worker);
+			worker.Name = "Vasiliy";
+
+			bool result = WorkerDao.updateEntity(worker);
+
+			Assert.IsTrue(result);
+			Assert.IsTrue(WorkerDao.selectEntityById(worker.Id).Name == "Vasiliy");
+		}
+
+		[Test, Order(4)]
+		public void deleteTest()
+		{
+			Assert.IsNotNull(worker);
+			bool result = WorkerDao.deleteEntityById(worker.Id);
+
+			Assert.IsTrue(result);
+			Assert.IsTrue(WorkerDao.selectEntities().Count == 0);
+		}
+	}
+}
